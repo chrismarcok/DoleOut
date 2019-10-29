89ea0cc646e5f9ed5593bfd13e1f09be6d3b2ff6
Christopher Marcok <chris.marcok@gmail.com>
2019-10-29 18:01:01 -0400

    buttons for the groups

--- src/comps/Group.js
+++ src/comps/Group.js
@@ -1,32 +1,36 @@
 import React from 'react'
+const Color = require('color');
 
 /* This is the group COMPONENT that is listed in the GroupPage*/
 
 class Group extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
-      hover: false
+      hover: false,
+      editHover: false,
+      deleteHover: false
     }
     this.toggleHover = this.toggleHover.bind(this)
   }
 
 
-  componentDidMount () {
+  componentDidMount() {
     const icon = document.querySelector("#icon" + this.props.id)
     icon.className = "group-icon fa fa-" + this.props.icon
     const group = document.querySelector(".group-div-id-" + this.props.id)
     group.style.backgroundColor = this.props.colorBg
+
   }
 
-  stringifyMembers(membersList){
-    if (membersList.length === 0){
+  stringifyMembers(membersList) {
+    if (membersList.length === 0) {
       return "No members."
     }
     let r = ""
-    for (let i = 0; i < membersList.length; i++){
+    for (let i = 0; i < membersList.length; i++) {
       r += membersList[i].username + ", "
     }
     return r.substring(0, r.length - 2)
@@ -43,25 +47,31 @@ class Group extends React.Component {
   render() {
 
     let colorBg
-    if (this.state.hover){
-      colorBg = {backgroundColor: '#ddd'}
+    if (this.state.hover) {
+      colorBg = { backgroundColor: Color(this.props.colorBg).darken(0.1).hsl().string() }
     } else {
-      colorBg = {backgroundColor: this.props.colorBg}
+      colorBg = { backgroundColor: this.props.colorBg }
     }
 
     return (
       <div>
         <a href={"/g/" + this.props.id}>
-        <div style={colorBg} className={"group-div group-div-id-" + this.props.id} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
-            
+          <div style={colorBg} className={"group-div group-div-id-" + this.props.id} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
+
             <h1 className="grouppage-title">
-            <i id={"icon" + this.props.id}></i> {this.props.name}
+              <i id={"icon" + this.props.id}></i> {this.props.name}
             </h1>
+            <div className="group-div-change-btns">
+              <div>
+                <i className="fa fa-edit" ></i>
+                <i className="fa fa-trash" ></i>
+              </div>
+            </div>
             <h3>
-              Members: { this.stringifyMembers(this.props.members) }
+              Members: {this.stringifyMembers(this.props.members)}
             </h3>
-        </div>   
-        </a>  
+          </div>
+        </a>
       </div>
     )
   }
